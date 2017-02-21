01611aed7cbafc7acae76b8828eb6751e07b4147
Tamas Papik <trapacska@gmail.com>
2017-02-21 13:00:01 +0100

    Deprecated message

--- git_clone.rb
+++ git_clone.rb
@@ -7,7 +7,8 @@ def log_error(message)
   puts "\e[31m#{message}\e[0m"
 end
 
-log_error('This step is deprecated, use activate-ssh-key + git-clone steps instead. Will be removed by: 2017-08-01')
+puts
+log_error('This step is deprecated, use activate-ssh-key + git-clone steps instead. Will be removed by: 2017-08-01.')
 
 options = {
 	user_home: ENV['HOME'],
